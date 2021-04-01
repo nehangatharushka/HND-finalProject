12a84e8f0395a629544a55fd1d8217746762a8c0
Nehanga <tharushka.nt@gmail.com>
2021-04-02 02:35:20 +0530

    v 2.5

--- app/src/main/java/com/example/weddingmallappilcation/home.java
+++ app/src/main/java/com/example/weddingmallappilcation/home.java
@@ -1,15 +1,12 @@
 package com.example.weddingmallappilcation;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class home extends AppCompatActivity {
 
-    private ImageButton newsFeed,account;
+//    private ImageButton newsFeed,account;
 
 
     @Override
@@ -17,16 +14,16 @@ public class home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        newsFeed=(ImageButton) findViewById(R.id.btnNewsFeed);
-        account=(ImageButton) findViewById(R.id.btnprofile);
-
-        account.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(home.this,account.class);
-                startActivity(intent);
-            }
-        });
+//        newsFeed=(ImageButton) findViewById(R.id.btnNewsFeed);
+//        account=(ImageButton) findViewById(R.id.btnprofile);
+//
+//        account.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(home.this,account.class);
+//                startActivity(intent);
+//            }
+//        });
 
 
 
